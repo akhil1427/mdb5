2b8c4a058766c1da680504871448525b44595ede
akhil1427 <akhilkonda61@gmail.com>
2023-12-04 11:11:54 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mdb5(request):
+    return render(request,'mdb5.html')
+
+
+def form(request):
+    return render(request,'form.html')
